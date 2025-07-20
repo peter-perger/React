2c0a5751825231dc2c611178257c7f0ddd4e79f7
peter-perger <pergerpeti2@gmail.com>
2025-07-20 18:58:29 +0200

    Convert home and checkout to async await

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Routes, Route } from 'react-router';
 import { useState, useEffect } from 'react';
 import { HomePage } from './pages/home/HomePage';
-import {CheckoutPage} from './pages/checkout/CheckoutPage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/orders/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
 import { NotFound } from './pages/NotFound';
@@ -12,20 +12,22 @@ function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/cart-items?expand=product')
-      .then((response) => {
+    const fetchAppData = async () => {
+      const response = await axios.get('/api/cart-items?expand=product')
         setCart(response.data);
-      });
+    };
+
+    fetchAppData();
   }, [])
-  
+
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart}/>}/>
-      <Route path="checkout" element={<CheckoutPage cart={cart}/>}/>
-      <Route path="orders" element={<OrdersPage cart={cart}/>}/>
-      <Route path='tracking' element={<TrackingPage/>}/>
-      <Route path='*' element={<NotFound/>}/>
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
+      <Route path='tracking' element={<TrackingPage />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   )
 }
