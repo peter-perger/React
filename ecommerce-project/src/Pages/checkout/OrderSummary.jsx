3356202bef5ba1fc73bca0149917da0955879024
peter-perger <pergerpeti2@gmail.com>
2025-07-21 12:32:48 +0200

    Separated delivery date into a component

--- ecommerce-project/src/Pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/Pages/checkout/OrderSummary.jsx
@@ -1,21 +1,13 @@
 import { CartItemDetails } from "./CartItemDetails";
 import { DeliveryOptions } from "./DeliveryOptions";
-import dayjs from "dayjs";
 
 export function OrderSummary( {cart, deliveryOptions} ) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 && cart.map((cartItem) => {
-        const selectedDeliveryOption = deliveryOptions
-          .find((deliveryOption) => {
-            return deliveryOption.id === cartItem.deliveryOptionId
-          });
-
         return (
           <div key={cartItem.productId} className="cart-item-container">
-            <div className="delivery-date">
-              Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-            </div>
+            <DeliveryOptions cartItem = {cartItem} deliveryOptions={deliveryOptions}/>
 
             <div className="cart-item-details-grid">
               <img className="product-image"
